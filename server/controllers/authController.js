8c0ba28d0cbf01ae385c3a6424d194dd8591384a
yakabomosr-ui <tonEmailGitHub@gmail.com>
2026-06-19 10:39:38 +0100

    Fix User model path for Linux

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user');
 
 exports.register = async (req, res) => {
   try {
